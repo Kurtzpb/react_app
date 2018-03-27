6d86abb8dde0ca8554447a1eaa2e705468d935e8
Aleksey Kuzmenko <kurtzpb@gmail.com>
2018-03-27 22:23:53 +0300

    in between commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'redux';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+@connect(store => {
+    return {}
+}) 
 
-export default App;
+export default class App extends Component {
+    render() {
+        return (
+        <div className="App">
+            <marquee style={{margin: '5px'}}>Loading...</marquee>
+            <h1>Hello!</h1>
+        </div>
+        );
+    }
+}
